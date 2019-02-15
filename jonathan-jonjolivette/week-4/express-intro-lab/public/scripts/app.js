908525ecf4beaaf190f0186a053b38625aef3726
JJ <johnhenry@JJs-MacBook-Pro.local>
2019-02-15 00:48:29 -0800

    developed the proper ajax callls and routes to populate object called upon to the browser, map function on 3 needs to be refactored to work properly

--- jonathan-jonjolivette/week-4/express-intro-lab/public/scripts/app.js
+++ jonathan-jonjolivette/week-4/express-intro-lab/public/scripts/app.js
@@ -1,25 +1,30 @@
 $( document ).ready( function() {
-   // console.log( 'Jquery is working!' )
-   const handleSuccess = ( res ) => {
-      $(".msg").text("ajax content successful");
+   const handleSuccess = json => {
+      json.map( (item) => {
+         $(".msg").append(`<div>${item.title}</div>`)
+      })
    };
-   const handleError = ( xhr, status, errorThrown ) => console.log( status );
-   let test =
+   // const success = json =>  $(".msg").append(handleSuccess);
+   const handleError = (xhr, status, errorThrown) => console.log('uh oh');
+
+
      $.ajax( {
         method: 'GET',
         url: 'http://localhost:3000/api/albums',
         success: handleSuccess,
         error: handleError
-     } );
+     });
 
-// Dynamically creating dom elements to inject page with the ajax content
+   //   In your app.js file, write a jQuery AJAX request to get the taqueria data. 
+   //   When the response comes back, display all the taqueria names above the albums on your site's root page (localhost:3000/).
 
+     $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/api/taquerias',
+        success: handleSuccess,
+        error: handleError
+     });
+
+console.log( "Sanity Check: JS in app.js is working!" );
 
 }); //document.ready concludes here
-// console.log( "Sanity Check: JS in app.js is working!" );
-
-
-
-// $(document).ajaxSuccess(function() {
-//   $( ".log" ).text( "Triggered ajaxSuccess handler." );
-// });
